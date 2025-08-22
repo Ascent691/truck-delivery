db8670b85ef617aaf71c67da9e5b50e6b4b35ba7
SeanNeal572 <sean.neal@chillisoft.co.za>
2025-08-22 19:58:47 +0200

    Minor refactor

--- TruckDelivery/TruckDelivery.Infrastructure/TollCalculator.cs
+++ TruckDelivery/TruckDelivery.Infrastructure/TollCalculator.cs
@@ -8,7 +8,7 @@
         {
             return deliveries.Select((delivery) => 
             {
-                var path = GetPossiblePaths(delivery.FromCityId, 1, []).First();
+                var path = GetPossiblePaths(delivery.FromCityId, 1, []);
                 _knownPaths.TryAdd(delivery.FromCityId, path);
 
                 return path.Where((road) => delivery.LoadWeight >= road.LoadLimit)
@@ -16,17 +16,17 @@
             });
         }
 
-        private IEnumerable<IEnumerable<Road>> GetPossiblePaths(long fromCity, long toCity, IEnumerable<Road> travelledRoads)
+        private IEnumerable<Road> GetPossiblePaths(long fromCity, long toCity, IEnumerable<Road> travelledRoads)
         {
-            if (_knownPaths.TryGetValue(fromCity, out var path)) return [travelledRoads.Concat(path)];
+            if (_knownPaths.TryGetValue(fromCity, out var path)) return travelledRoads.Concat(path);
 
-            if (fromCity == toCity) return [travelledRoads];
+            if (fromCity == toCity) return travelledRoads;
 
             var connectedRoads = roads.Where((road) => road.FirstCityId == fromCity || road.SecondCityId == fromCity);
             var untravelledRoads = connectedRoads.Where((road) => !travelledRoads.Contains(road));
             
-            return untravelledRoads.SelectMany((road) => GetPossiblePaths(GetDestination(fromCity, road), toCity, [..travelledRoads, road]))
-                .Where((paths) => paths.Any());
+            return untravelledRoads.Select((road) => GetPossiblePaths(GetDestination(fromCity, road), toCity, [..travelledRoads, road]))
+                .First((paths) => paths.Any());
         }
 
         private long GetDestination(long fromCity, Road road)
