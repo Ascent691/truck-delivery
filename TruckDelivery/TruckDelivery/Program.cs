42e7c5b7503df302fe69a025f050a3baeb33a725
Predeshen Naidoo <predeshen.naidoo@chillisoft.co.za>
2025-09-05 15:05:40 +0200

    trying out Parallel for , down to 2.5seconds

--- TruckDelivery/TruckDelivery/Program.cs
+++ TruckDelivery/TruckDelivery/Program.cs
@@ -47,7 +47,7 @@ namespace TruckDelivery
             Console.WriteLine($"Execution Time: {timer.Elapsed}");
             Console.ReadLine();
         }
-        private static ScenarioAnswer DetermineAnswerOld(Scenario scenario)
+        private static ScenarioAnswer DetermineAnswerPreOld(Scenario scenario)
         {
             var allRoads = scenario.Roads.ToList();
             var finalGcdValues = new List<long>();
@@ -109,7 +109,7 @@ namespace TruckDelivery
             return new ScenarioAnswer(finalGcdValues.ToArray());
         }
 
-        private static ScenarioAnswer DetermineAnswer(Scenario scenario)
+        private static ScenarioAnswer DetermineAnswerAi(Scenario scenario)
         {
             var adjacency = new Dictionary<long, List<Road>>();
             foreach (var road in scenario.Roads)
@@ -179,5 +179,74 @@ namespace TruckDelivery
             return new ScenarioAnswer(finalGcdValues.ToArray());
         }
 
+        //Using Parallel
+        record Neighbor(long CityId, long LoadLimit, long TollCharge);
+
+        private static ScenarioAnswer DetermineAnswer(Scenario scenario)
+        {
+            var adjacency = new Dictionary<long, List<Neighbor>>();
+            long maxCityId = 0;
+
+            foreach (var road in scenario.Roads)
+            {
+                if (!adjacency.ContainsKey(road.FirstCityId))
+                    adjacency[road.FirstCityId] = new List<Neighbor>();
+                if (!adjacency.ContainsKey(road.SecondCityId))
+                    adjacency[road.SecondCityId] = new List<Neighbor>();
+
+                adjacency[road.FirstCityId].Add(new Neighbor(road.SecondCityId, road.LoadLimit, road.TollCharge));
+                adjacency[road.SecondCityId].Add(new Neighbor(road.FirstCityId, road.LoadLimit, road.TollCharge));
+
+                if (road.FirstCityId > maxCityId) maxCityId = road.FirstCityId;
+                if (road.SecondCityId > maxCityId) maxCityId = road.SecondCityId;
+            }
+
+            var finalGcdValues = new long[scenario.Deliveries.Count()];
+
+            Parallel.ForEach(
+                scenario.Deliveries.Select((delivery, index) => (delivery, index)),
+                tuple =>
+                {
+                    var (delivery, index) = tuple;
+
+                    var visited = new bool[maxCityId + 1];
+                    var tolls = new HashSet<long>();
+
+                    long currentCity = delivery.FromCityId;
+                    long destination = delivery.ToCityId;
+                    long deliveryWeight = delivery.LoadWeight;
+
+                    visited[currentCity] = true;
+
+                    while (currentCity != destination)
+                    {
+                        bool foundNextRoad = false;
+
+                        foreach (var neighbor in adjacency[currentCity])
+                        {
+                            if (visited[neighbor.CityId]) continue;
+
+                            if (deliveryWeight >= neighbor.LoadLimit)
+                                tolls.Add(neighbor.TollCharge);
+
+                            currentCity = neighbor.CityId;
+                            visited[currentCity] = true;
+                            foundNextRoad = true;
+                            break;
+                        }
+
+                        if (!foundNextRoad)
+                            break;
+                    }
+
+                    long dailyGcd = 0;
+                    foreach (var toll in tolls)
+                        dailyGcd = dailyGcd == 0 ? toll : MathHelper.GreatestCommonDivisor(dailyGcd, toll);
+
+                    finalGcdValues[index] = dailyGcd;
+                });
+
+            return new ScenarioAnswer(finalGcdValues);
+        }
     }
 }
